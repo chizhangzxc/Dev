c9bb5241e9a469202514e71a48a24cae42d2d0b0
chizhangzxc <78051160+chizhangzxc@users.noreply.github.com>
2023-08-18 14:25:31 -0400

    Problem 34

---
+++ 34.mjs
@@ -0,0 +1,22 @@
+const factorial = function(n) 
+{
+    if(n == 0 | n == 1)
+    {
+        return 1
+    }
+    else
+    {
+        return(n*factorial(n-1))
+    }
+}
+
+let nums = []
+for(let i = 3; i < 10000000; i++)
+{
+    let sum = i.toString().split("").map(x => factorial(x)).reduce((a,b) => a+b, 0)
+    if(sum == i)
+    {
+        nums.push(i)
+    }
+}
+console.log(nums.reduce((a,b) => a+b, 0))
